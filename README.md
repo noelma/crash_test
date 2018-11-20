2706713583e20d3dda8c5a58d12000af58637073
noelma <noel-mathieu@hotmail.fr>
2018-11-20 10:38:37 +0100

    no message

---
+++ README.md
@@ -0,0 +1,55 @@
+# Soosyze Framework
+
+[![Build Status](https://travis-ci.org/soosyze/framework.svg?branch=master)](https://travis-ci.org/soosyze/framework)
+[![Coverage Status](https://coveralls.io/repos/github/soosyze/framework/badge.svg?branch=master)](https://coveralls.io/github/soosyze/framework?branch=master)
+![GitHub](https://img.shields.io/github/license/mashape/apistatus.svg)
+![Packagist](https://img.shields.io/packagist/v/soosyze/framework.svg)
+![PHP from Packagist](https://img.shields.io/packagist/php-v/soosyze/framework.svg)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/soosyze/framework.svg)
+
+Soosyze Framework est un micro-framework MVC object offrant un socle solide de développement
+
+* ![PSR-2](https://img.shields.io/badge/PSR-2-yellow.svg) L'écriture du code est standardisé,
+* ![PSR-4](https://img.shields.io/badge/PSR-4-yellow.svg) Autoloader, interchangeable avec l'autoloader de compsoser,
+* ![PSR-7](https://img.shields.io/badge/PSR-7-yellow.svg) Requête et Réponse,
+* ![PSR-11](https://img.shields.io/badge/PSR-11-yellow.svg) Container d'injection de dépendance ou CID,
+* Découpe des fonctionnalitées en modules,
+* Routeur (url),
+* Hook et Middleware,
+* Controlleur,
+* Composant d'aide au développement
+    * Création de formulaire,
+    * Validateur de données,
+    * Envoie de Mail,
+    * Création de Templace.
+
+# Sommaire
+
+* [Requirements](/README.md#requirements)
+* [Installation](/README.md#installation)
+* [License](/README.md#license)
+
+# Requirements
+
+* PHP =>5.4, support PHP 5.6, 7.0, 7.1
+* La permission d'écrire et lire les fichiers (Si vous utilisez le composant Util),
+* L'extension `json` activé (Si vous utilisez le composant Util).
+
+
+# Installation
+
+## Composer
+
+Vous pouvez utiliser [Composer](https://getcomposer.org/) pour l'installation avec la commande suivante :
+```sh
+composer require soosyze/framework
+```
+
+Ou, si vous utilisez le PHAR (assurez-vous que l'exécutable php.exe est dans votre PATH):
+```sh
+php composer.phar require soosyze/framework
+```
+
+# License
+
+Soosyze Framework est sous licence MIT. Voir le fichier de licence pour plus d'informations.
---
+++ src/Controller.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * Soosyze Framework http://soosyze.com
+ *
+ * @package Soosyze
+ * @author  Mathieu NOËL <mathieu@soosyze.com>
+ * @license https://github.com/soosyze/framework/blob/master/LICENSE (MIT License)
+ */
+
+namespace Soosyze;
+
+use Soosyze\Components\Http\Reponse;
+use Soosyze\Components\Http\Stream;
+
+/**
+ * Les méthodes du contrôleur sont appelées à partir d'une requête http
+ * puis retourne une réponse.
+ *
+ * Le contrôleur est un élément essentiel du schéma MVC,
+ * il fait le lien entre votre modèle de données et vos vues.
+ * Il est l'élément de base de vos modules.
+ *
+ * @author Mathieu NOËL
+ */
+class Controller
+{
+    /**
+     * Chemin du fichier contenant les routes.
+     *
+     * @var string
+     */
+    protected $pathRoutes = '';
+
+    /**
+     * Chemin du fichier contenant les services.
+     *
+     * @var string
+     */
+    protected $pathServices = '';
+
+    /**
+     * Container d'injection de dépendance (CID).
+     *
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * Appel un service directement par son nom.
+     *
+     * @codeCoverageIgnore Fonction testé directement avec l'objet Container.
+     *
+     * @param string $name Nom du service.
+     * @param array $arg Paramètres passés à la fonction.
+     *
+     * @return object
+     */
+    public function __call($name, $arg)
+    {
+        return $this->get($name);
+    }
+
+    /**
+     * Ajoute le container d'injection de dépendance au contrôleur.
+     *
+     * @param Container $container CID.
+     */
+    public function setContainer($container)
+    {
+        $this->container = $container;
+
+        return $this;
+    }
+
+    /**
+     * Appel le container.
+     *
+     * @codeCoverageIgnore Fonction testé directement avec l'objet Container.
+     *
+     * @param string $key Nom du service.
+     *
+     * @return object Service dans le container.
+     */
+    public function get($key)
+    {
+        return $this->container->get($key);
+    }
+
+    /**
+     * Retourne le chemin du fichier de configuration des routes.
+     *
+     * @return string
+     */
+    public function getPathRoutes()
+    {
+        return $this->pathRoutes;
+    }
+
+    /**
+     * Retourne le chemin du fichier de configuration des services.
+     *
+     * @return string
+     */
+    public function getPathServices()
+    {
+        return $this->pathServices;
+    }
+
+    /**
+     * Retourne une réponse avec le statut 404.
+     *
+     * @codeCoverageIgnore Fonction testé directement avec l'objet Reponse.
+     *
+     * @param string $stream
+     * @param RequestInterface $request
+     *
+     * @return Reponse
+     */
+    protected function get404($stream = null, RequestInterface $request = null)
+    {
+        $text = 'Page %s Not Found, Sorry, but the page you were trying to view does not exist.';
+        $stream = $stream instanceof RequestInterface && $request === null
+            ? sprintf($text, $stream->getUri()->getQuery())
+            : sprintf($text, '');
+        
+        return new Reponse(404, new Stream($stream));
+    }
+}
